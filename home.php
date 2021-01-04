83c7f74f160cad004701fabd37f76995f257872c
qsdMonkey <72322767+qsdMonkey@users.noreply.github.com>
2021-01-04 13:59:11 +0100

    side bar

--- home.php
+++ home.php
@@ -27,41 +27,25 @@
         </div>
 
         <ul class="list-unstyled components">
-            <p>Dummy Heading</p>
+            <li>
+                <a href="#">Acceuil</a>
+            </li>
+            <li>
+                <a href="#">Magasin</a>
+            </li>
             <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Genre</a>
                 <ul class="collapse list-unstyled" id="homeSubmenu">
                     <li>
-                        <a href="#">Home 1</a>
+                        <a href="#">RPG</a>
                     </li>
                     <li>
-                        <a href="#">Home 2</a>
+                        <a href="#">FPS</a>
                     </li>
                     <li>
-                        <a href="#">Home 3</a>
+                        <a href="#">Open World</a>
                     </li>
                 </ul>
-            </li>
-            <li>
-                <a href="#">About</a>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li>
-                        <a href="#">Page 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 3</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Portfolio</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
             </li>
         </ul>
     </nav>
