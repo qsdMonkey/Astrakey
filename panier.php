8a3d53aa86f96919eb53652848cdfef6aa263f53
Gh0st-cmd <61194866+Gh0st-cmd@users.noreply.github.com>
2021-01-04 14:17:13 +0100

    Update panier.php

--- panier.php
+++ panier.php
@@ -19,7 +19,41 @@
 </head>
 <body>
 
+      <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+          <div class="container-fluid">
 
+              <button type="button" id="sidebarCollapse" class="navbar-btn">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+              </button>
+              <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <i class="fas fa-align-justify"></i>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="nav navbar-nav ml-auto">
+
+                      <img src="./img/Logo.png" alt="logo" style="height: 70px" >
+                      <img src="./img/Astrakey.png" alt="logo" style="height: 80px" >
+
+                      <li class="nav-item active">
+                          <a class="nav-link" href="home.php">Accueil</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="magasin.php">Magasin</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="panier.php">Panier</a>
+                      </li>
+                      <form class="form-inline">
+                          <input class="form-control mr-sm-2 navbar-text" type="search" placeholder="Rechercher" aria-label="Rechercher">
+                          <button class=" btn btn-outline-light my-2 my-sm-0 " type="submit">Rechercher</button>
+                      </form>
+                  </ul>
+              </div>
+          </div>
+      </nav>
 
 
 
