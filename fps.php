12b6a51b89f18dd2d6d35e213c3ccde759d325c9
qsdMonkey <72322767+qsdMonkey@users.noreply.github.com>
2021-01-06 16:16:45 +0100

    Post formulaire

--- fps.php
+++ fps.php
@@ -111,9 +111,12 @@ try {
                         <h5 class="card-title navbar-text"><?php echo $game["name"];?></h5>
                         <h5 class="card-title texte-price"><?php echo $game["price"]." €";?></h5>
                     </div>
-                    <div class="card-footer">
-                        <button type="button" class="btn btn-primary">Acheter</button>
-                    </div>
+                    <form class="" action="game.php" method="post">
+                        <div class="card-footer">
+                            <input type="hidden" name="id_games" value ='<?php echo $game["id_games"]?>'>
+                            <button type="submit" href="game.php" class="btn btn-primary">Acheter</button>
+                        </div>
+                    </form>
                 </div>
             </div>
 
