8fd64e3a1568b49bbc0c8100e2a84c48b295a9ef
Gh0st-cmd <61194866+Gh0st-cmd@users.noreply.github.com>
2021-01-08 09:02:38 +0100

    Update openworld.php

--- openworld.php
+++ openworld.php
@@ -3,7 +3,7 @@
 <?php
 session_start();
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=astrakey', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=astrakey', 'root', 'root');
 
 } catch (Exception $e) {
     die('Error');
